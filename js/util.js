49f1622c31641233568203972dcedc1d58150689
ratherblue <ratherblue@gmail.com>
2016-02-02 15:31:09 -0800

    adding more functions to test

--- js/util.js
+++ js/util.js
@@ -1,8 +1,42 @@
 var Util = {
+  // perform some random math
+  randomMath: function(num1, num2) {
+    if (num1 < num2) {
+      return num2 - num1;
+    }
+    else if (num1 > num2) {
+      return num1 * num2;
+    }
+    else {
+      return num1 + num2;
+    }
+  },
 
+  // Adds up the length of each word in the array
+  addWordLengths: function(array) {
+    var count = 0;
 
+    for (var x = 0; x < array.length; x++) {
+      count += array[x].length;
+    }
 
+    return count;
+  },
+  // Counts the number of times a letter appears in an array
+  countLettersInArray: function(array, letter) {
+    return Util.countLettersInString(array.join(''), letter);
+  },
+  // Counts the number of times a letter appears in a string
+  countLettersInString: function(string, letter) {
+    var matches = string.match(new RegExp(letter, 'gi'));
 
-
-
+    if (matches !== null) {
+      return matches.length;
+    } else {
+      return 0;
+    }
+  }
 };
+
+
+module.exports = Util;
--- test/util.js
+++ test/util.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-
+var Util = require('../js/util');
 
 describe('util.js', function() {
 
